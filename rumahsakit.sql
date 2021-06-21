3349ecfc40aa4e5c5400a09f594e6c679422496b
agin123633 <aginbugin11@gmail.com>
2021-06-21 14:29:50 +0700

    menambah file

---
+++ rumahsakit.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 20, 2021 at 12:09 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rumahsakit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_dokter`
+--
+
+CREATE TABLE `tb_dokter` (
+  `id_dokter` varchar(50) NOT NULL,
+  `nama_dokter` varchar(80) NOT NULL,
+  `spesialis` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_obat`
+--
+
+CREATE TABLE `tb_obat` (
+  `id_obat` varchar(50) NOT NULL,
+  `nama_obat` varchar(200) NOT NULL,
+  `keterangan_obat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pasien`
+--
+
+CREATE TABLE `tb_pasien` (
+  `id_pasien` varchar(50) NOT NULL,
+  `nomor_identitas` varchar(30) NOT NULL,
+  `nama_pasien` varchar(80) NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_poliklinik`
+--
+
+CREATE TABLE `tb_poliklinik` (
+  `id_poli` varchar(50) NOT NULL,
+  `nama_poli` varchar(50) NOT NULL,
+  `nama_gedung` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rekammedis`
+--
+
+CREATE TABLE `tb_rekammedis` (
+  `id_rm` varchar(50) NOT NULL,
+  `id_pasien` varchar(50) NOT NULL,
+  `keluhan` text NOT NULL,
+  `id_dokter` varchar(50) NOT NULL,
+  `diagnosa` text NOT NULL,
+  `id_poli` varchar(50) NOT NULL,
+  `tgl_periksa` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rm_obat`
+--
+
+CREATE TABLE `tb_rm_obat` (
+  `id` int(8) NOT NULL,
+  `id_rm` varchar(50) NOT NULL,
+  `id_obat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` varchar(50) NOT NULL,
+  `nama_user` varchar(80) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` enum('1','2') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_dokter`
+--
+ALTER TABLE `tb_dokter`
+  ADD PRIMARY KEY (`id_dokter`);
+
+--
+-- Indexes for table `tb_obat`
+--
+ALTER TABLE `tb_obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indexes for table `tb_pasien`
+--
+ALTER TABLE `tb_pasien`
+  ADD PRIMARY KEY (`id_pasien`);
+
+--
+-- Indexes for table `tb_poliklinik`
+--
+ALTER TABLE `tb_poliklinik`
+  ADD PRIMARY KEY (`id_poli`);
+
+--
+-- Indexes for table `tb_rekammedis`
+--
+ALTER TABLE `tb_rekammedis`
+  ADD PRIMARY KEY (`id_rm`),
+  ADD KEY `id_pasien` (`id_pasien`),
+  ADD KEY `id_dokter` (`id_dokter`),
+  ADD KEY `id_poli` (`id_poli`);
+
+--
+-- Indexes for table `tb_rm_obat`
+--
+ALTER TABLE `tb_rm_obat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_rm` (`id_rm`),
+  ADD KEY `id_obat` (`id_obat`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_rekammedis`
+--
+ALTER TABLE `tb_rekammedis`
+  ADD CONSTRAINT `tb_rekammedis_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `tb_pasien` (`id_pasien`),
+  ADD CONSTRAINT `tb_rekammedis_ibfk_2` FOREIGN KEY (`id_dokter`) REFERENCES `tb_dokter` (`id_dokter`),
+  ADD CONSTRAINT `tb_rekammedis_ibfk_3` FOREIGN KEY (`id_poli`) REFERENCES `tb_poliklinik` (`id_poli`);
+
+--
+-- Constraints for table `tb_rm_obat`
+--
+ALTER TABLE `tb_rm_obat`
+  ADD CONSTRAINT `tb_rm_obat_ibfk_1` FOREIGN KEY (`id_rm`) REFERENCES `tb_rekammedis` (`id_rm`),
+  ADD CONSTRAINT `tb_rm_obat_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `tb_obat` (`id_obat`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
